554e33d575cff500b0a0fef03e38b3fdeb030a74
matias1364 <matiasleoncontreras@gmail.com>
2023-05-05 21:55:30 -0300

    first

---
+++ Peluq.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.peluq;
+
+import com.mycompany.peluq.inter.Principal;
+
+/**
+ *
+ * @author Matias
+ */
+public class Peluq {
+
+    public static void main(String[] args) {
+        Principal princi = new Principal();
+        princi.setVisible(true);
+        princi.setLocationRelativeTo(null);
+        
+    }
+}
